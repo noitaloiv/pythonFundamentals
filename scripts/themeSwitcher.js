b5205fd89d10b7b93e2b62da405f92e324046ac5
Mamba <backeanton@gmail.com>
2024-02-19 22:36:35 +0100

    Update themeSwitcher.js

--- scripts/themeSwitcher.js
+++ scripts/themeSwitcher.js
@@ -3,9 +3,9 @@ const toggleDarkMode = document.querySelector('.js-toggle-dark-mode');
 jtd.addEvent(toggleDarkMode, 'click', function(){
 if (jtd.getTheme() === 'dark' || jtd.getTheme() === 'default') {
 	jtd.setTheme('light');
-	toggleDarkMode.innerHTML = "<i class="fa solid fa-moon fa-3x">";
+	toggleDarkMode.innerHTML = "<i class='fa solid fa-moon fa-3x'>";
 } else {
 	jtd.setTheme('dark');
-	toggleDarkMode.innerHTML = "<i class="fa solid fa-sun fa-3x">";
+	toggleDarkMode.innerHTML = "<i class='fa solid fa-sun fa-3x'>";
 	}
 });
