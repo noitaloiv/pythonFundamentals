5fd7dff1afd0246fb5918c8d461ad6db7808616d
Mamba <backeanton@gmail.com>
2025-06-04 01:20:47 +0200

    Dark mode logo fix

--- assets/js/theme-switcher.js
+++ assets/js/theme-switcher.js
@@ -1,33 +1,39 @@
-window.addEventListener("DOMContentLoaded", function() {
+window.addEventListener("DOMContentLoaded", function () {
   const toggleDarkMode = document.getElementById("theme-toggle");
+  //const themeIcon = document.getElementById("theme-icon");
+  const mainLogo = document.getElementById("main-logo");
 
-  if (localStorage.getItem('theme') === 'dark') {
-    setTheme('dark');
+  if (localStorage.getItem("theme") === "dark") {
+    setTheme("dark");
   } else {
-    setTheme('light');
+    setTheme("light");
   }
 
-  jtd.addEvent(toggleDarkMode, 'click', function(){
+  jtd.addEvent(toggleDarkMode, "click", function () {
     const currentTheme = getTheme();
-    const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+    const newTheme = currentTheme === "dark" ? "light" : "dark";
 
-    localStorage.setItem('theme', newTheme);
+    localStorage.setItem("theme", newTheme);
     setTheme(newTheme);
   });
 
   function getTheme() {
-    return document.documentElement.classList.contains('dark-mode') ? 'dark' : 'light';
+    return document.documentElement.classList.contains("dark-mode")
+      ? "dark"
+      : "light";
   }
 
   function setTheme(theme) {
-    if (theme === 'dark') {
-      toggleDarkMode.innerHTML = `<svg width='18px' height='18px'><use href="#svg-moon"></use></svg>`;
-      document.documentElement.classList.add('dark-mode');
-      document.documentElement.classList.remove('light-mode');
+    if (theme === "dark") {
+      toggleDarkMode.innerHTML = `<svg width='18px' height='18px'><use href="#sun"></use></svg>`;
+      mainLogo.src = "/assets/images/logo_white.svg";
+      document.documentElement.classList.add("dark-mode");
+      document.documentElement.classList.remove("light-mode");
     } else {
-      toggleDarkMode.innerHTML = `<svg width='18px' height='18px'><use href="#svg-sun"></use></svg>`;
-      document.documentElement.classList.add('light-mode');
-      document.documentElement.classList.remove('dark-mode');
+      toggleDarkMode.innerHTML = `<svg width='18px' height='18px'><use href="#moon"></use></svg>`;
+      mainLogo.src = "/assets/images/logo_black.svg";
+      document.documentElement.classList.add("light-mode");
+      document.documentElement.classList.remove("dark-mode");
     }
   }
 });
